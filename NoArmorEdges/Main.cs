f55a09c6aaa9b024cb2399cd7b63ce7b659cb8bd
austinvaness <austinvaness@gmail.com>
2021-05-30 10:55:25 -0500

    Initial commit

---
+++ NoArmorEdges/Main.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using VRage.Plugins;
+
+namespace avaness.NoArmorEdges
+{
+    public class Main : IPlugin
+    {
+        public void Dispose()
+        {
+
+        }
+
+        public void Init(object gameInstance)
+        {
+            Harmony harmony = new Harmony("avaness.NoArmorEdges");
+            harmony.PatchAll();
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
---
+++ NoArmorEdges/Patch_DisableEdges.cs
@@ -0,0 +1,11 @@
+﻿using HarmonyLib;
+using Sandbox.Game.Entities;
+
+[HarmonyPatch(typeof(MyCubeGrid), "AddBlockEdges")]
+public static class Patch_DisableEdges
+{
+    public static bool Prefix()
+    {
+        return false;
+    }
+}
